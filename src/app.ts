0a2d57c96c669e85e6a7c1ba9892aaa284a1c86b
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-10-20 16:55:18 +0530

    chore: change end-point path

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import Routes from "./routes/index.route";
+import routes from "./routes/index.route";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use("/", Routes);
+app.use("/", routes);
 app.use(express.json());
 
 app.listen(port, () => {
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -6,7 +6,7 @@ const router = express.Router();
  * Add all routes below
  * For example, below
  */
-router.use("/ping", TestRoute);
+router.use("/", TestRoute);
 
 router.use("/some-another-route", (req, res) => {
   res.json({ message: "Some route" });
