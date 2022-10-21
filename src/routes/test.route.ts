9900cfc9c70e1d0092c426e3c8ef68b6e950513d
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-10-21 12:18:49 +0530

    chore: add jsonwebtken npm packages and types

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.json({ ping: "Pong! 😈", message: "Welcome to FraMan API Server 😂" });
 });
 
+router.get("/other", (req, res) => {
+  res.json({ some: "other route" });
+});
+
 export default router;
