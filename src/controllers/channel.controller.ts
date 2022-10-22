464ff686176579df14cf3af0592b00465f18267d
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-10-22 12:24:50 +0530

    feat: add count key to api response

--- src/controllers/channel.controller.ts
+++ src/controllers/channel.controller.ts
@@ -4,7 +4,12 @@ import { getAll } from "../models/direct-channel.model";
 export const getDirectChannels = async (req: Request, res: Response) => {
   try {
     const directChannels = await getAll();
-    res.status(200).json({ status: 0, data: { directChannels } });
+    res
+      .status(200)
+      .json({
+        status: 0,
+        data: { count: directChannels.length, directChannels },
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: 1, data: { message: error.message } });
