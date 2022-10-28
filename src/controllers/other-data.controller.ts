9589985701210314e5bc3b5689f28621179883c2
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-10-28 11:54:56 +0530

    feat: add api to fetch client account types

--- src/controllers/other-data.controller.ts
+++ src/controllers/other-data.controller.ts
@@ -1,9 +1,19 @@
 import { getAll } from "../models/nationality.model";
+import { getAll as getAllClientAccountTypes } from "../models/client-account-type.model";
 
 export const getAllNationalities = async (req, res) => {
   try {
     const nationalities = await getAll();
     res.status(200).json({ status: 0, data: { nationalities } });
+  } catch (error) {
+    res.status(200).json({ status: 1, data: { message: error.message } });
+  }
+};
+
+export const getClientAccountTypes = async (req, res) => {
+  try {
+    const clientAccountTypes = await getAllClientAccountTypes();
+    res.status(200).json({ status: 0, data: { clientAccountTypes } });
   } catch (error) {
     res.status(200).json({ status: 1, data: { message: error.message } });
   }
--- src/routes/other-data.route.ts
+++ src/routes/other-data.route.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { getAllNationalities } from "../controllers/other-data.controller";
+import {
+  getAllNationalities,
+  getClientAccountTypes,
+} from "../controllers/other-data.controller";
 const router = express.Router();
 
 router.post("/nationalities", getAllNationalities);
+router.post("/client-account-types", getClientAccountTypes);
 
 export const OtherDataRouter = router;
