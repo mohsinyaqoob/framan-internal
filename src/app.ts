96e5ca937ae9d66dd4bed6c4cc9fb3050d4b35e8
Mohsin Yaqoob <mohsinyqb@gmail.com>
2022-11-02 15:50:46 +0530

    feat: add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.route";
 import bootstrap from "./bootstrap";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Create all routes in ./routes/index.ts
 app.use("/", routes);
